258c3120d79e582c5670c7d395cef60e615f559a
Tiago de Mello Britto <obritto@hotmail.com>
2015-07-13 14:25:37 -0300

    TabbedPageApp

---
+++ TabbledPageApp/TabbledPageApp/Page2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TabbledPageApp
+{
+    public class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            Title = "tab 3";
+
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage trtrtrtr" }
+                }
+            };
+        }
+    }
+}
---
+++ TabbledPageApp/TabbledPageApp/TabPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TabbledPageApp
+{
+    public class TabPage : TabbedPage
+    {
+        public TabPage()
+        {
+            this.Title = "TABS";
+
+            Children.Add(new Page
+            {
+                Title = "TAB 1",
+            });
+
+            Children.Add(new Page1());
+
+            Children.Add(new Page2());
+
+
+            this.SelectedItem = Children[2];
+
+        }
+    }
+}
